2089db5b5e555979298fa0b6d89064cb9465ced9
Joshua <atherleyjoshua@gmail.com>
2021-12-06 11:54:15 -0400

    Add files via upload

---
+++ javaindepth/src/com/semanticsquare/basics/HelloWorld.java
@@ -0,0 +1,11 @@
+//download JDK 17 64 installer then set environment variables in the second half 
+//Make this folder C:\javaindepth\src\com\semanticsquare\basics
+// C:\javaindepth\src\com\semanticsquare\basics
+
+class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("HelloWorld!!");
+	
+	}
+}
